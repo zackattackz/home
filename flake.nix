e2ef562aabdc293bbc71ca15b98ed96a5dbc7492
Zachary Hanham <z.hanham00@gmail.com>
2023-10-22 12:31:33 -0400

    zaha-odoo user

--- flake.nix
+++ flake.nix
@@ -18,26 +18,33 @@
         config.allowUnfree = true;
         config.allowUnfreePredicate = pkg: true;
       };
+      homeArgs = usernameInput: rec {
+        homeFilesPath = ./files/home;
+        homeModulesPath = ./modules/home;
+        globals = rec {
+          username = usernameInput;
+          homeDirectory = "/home/${username}";
+          fontFamily = "Iosevka NFM";
+          wallpaper = "aurora1.png";
+          picturesPath =  "${homeDirectory}/pictures";
+          wallpaperPath = "${picturesPath}/wallpapers/${wallpaper}";
+          filesPath = homeFilesPath;
+        };
+      };
     in {
       homeConfigurations."zaha" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         modules = [
           ./users/zaha.nix
         ];
-        extraSpecialArgs = rec {
-          homeFilesPath = ./files/home;
-          homeModulesPath = ./modules/home;
-          globals = rec {
-            username = "zaha";
-            homeDirectory = "/home/${username}";
-            fontFamily = "Iosevka NFM";
-            wallpaper = "aurora1.png";
-            picturesPath =  "${homeDirectory}/pictures";
-            wallpaperPath = "${picturesPath}/wallpapers/${wallpaper}";
-            filesPath = homeFilesPath;
-          };
-
-        };
+        extraSpecialArgs = homeArgs "zaha";
+      };
+      homeConfigurations."zaha-odoo" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          ./users/zaha-odoo.nix
+        ];
+        extraSpecialArgs = homeArgs "zaha-odoo";
       };
       nixosConfigurations."nyx" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
