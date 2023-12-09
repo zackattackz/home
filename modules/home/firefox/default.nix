cdf0b06023d571a1c9e91e9a9df2ed7cbe3cc550
Zachary Hanham <z.hanham00@gmail.com>
2023-12-09 16:50:29 -0500

    firefox settings (hardware acell)

--- modules/home/firefox/default.nix
+++ modules/home/firefox/default.nix
@@ -10,6 +10,15 @@ in
   config = mkIf cfg.enable {
     programs.firefox = {
       enable = true;
+      profiles.personal = {
+        id = 0;
+        settings = {
+          "gfx.webrender.all" = true; # Force enable GPU acceleration
+          "media.ffmpeg.vaapi.enabled" = true;
+          "widget.dmabuf.force-enabled" = true; # Required in recent Firefoxes
+          "privacy.webrtc.legacyGlobalIndicator" = false;
+        };
+      };
     };
   };
 }
