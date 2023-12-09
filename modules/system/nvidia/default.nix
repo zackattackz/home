bae967e41e5b79c71be5e11041a6b807e4eb87c7
Zachary Hanham <z.hanham00@gmail.com>
2023-12-09 16:52:07 -0500

    nvidia env vars / full comp pipeline

--- modules/system/nvidia/default.nix
+++ modules/system/nvidia/default.nix
@@ -10,12 +10,22 @@ in
     enable = mkEnableOption "nvidia options";
   };
   config = mkIf cfg.enable {
+    environment.variables = {
+      LIBVA_DRIVER_NAME = "nvidia";
+      GBM_BACKEND = "nvidia-drm";
+      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+      WLR_NO_HARDWARE_CURSORS = "1";
+      MOZ_DISABLE_RDD_SANDBOX = "1";
+      NVD_BACKEND = "direct";
+    };
+
     hardware.nvidia = {
       modesetting.enable = true;
       powerManagement.enable = true;
       open = false;
       nvidiaSettings = true;
       package = config.boot.kernelPackages.nvidiaPackages.stable;
+      forceFullCompositionPipeline = true;
     };
 
     services.xserver = {
