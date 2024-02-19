cb54088ce936526573be4014f15d36c7d82e1034
Zachary Hanham <z.hanham00@gmail.com>
2024-02-18 21:08:15 -0500

    snowdoo-support

--- flake.nix
+++ flake.nix
@@ -3,11 +3,12 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    snowdoo-support.url = "git+ssh://git@github.com/zaha-odoo/snowdoo-support";
   };
 
   outputs = { nixpkgs, home-manager, ... }:
