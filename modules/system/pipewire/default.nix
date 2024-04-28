6ed08071c37fc1a18d528206e4d7113c96d93247
Zachary Hanham <z.hanham00@gmail.com>
2024-04-28 15:51:12 -0400

    pipewire updates

--- modules/system/pipewire/default.nix
+++ modules/system/pipewire/default.nix
@@ -8,8 +8,13 @@ in
 {
   options.pipewire.enable = mkEnableOption "pipewire options";
   config = mkIf cfg.enable {
+    environment.etc."openal/alsoft.conf".text = ''
+      [pulse]
+      allow-moves=true
+    '';
     services.pipewire = {
       enable = true;
+      audio.enable = true;
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
