0fe68640fb0adf3839e3b03fa7f732bf64ca00c4
Zachary Hanham <z.hanham00@gmail.com>
2024-10-31 13:52:14 -0400

    use grub

--- modules/system/bootloader/default.nix
+++ modules/system/bootloader/default.nix
@@ -11,7 +11,10 @@ in
     enableCryptoKeyfile = mkEnableOption "set boot.initrd.secrets crypto keyfile";
   };
   config = mkIf cfg.enable {
-    boot.loader.systemd-boot.enable = true;
+    boot.loader.grub.enable = true;
+    boot.loader.grub.device = "nodev";
+    boot.loader.grub.efiSupport = true;
+    boot.loader.grub.useOSProber = true;
     boot.loader.efi.canTouchEfiVariables = true;
     boot.initrd.secrets = mkIf cfg.enableCryptoKeyfile {
       "/crypto_keyfile.bin" = null;
